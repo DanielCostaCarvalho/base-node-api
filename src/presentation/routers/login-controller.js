b7a3aa444a27117343a8a54a2a56098e330254cb
Daniel Costa Carvalho <danielcostacarvalho@gmail.com>
2020-07-12 21:23:37 -0300

    Retorna 400 se não for fornecida senha

--- src/presentation/routers/login-controller.js
+++ src/presentation/routers/login-controller.js
@@ -1,6 +1,6 @@
 module.exports = () => {
   return async (httpRequest) => {
-    if (!httpRequest.body.email) {
+    if (!httpRequest.body.email || !httpRequest.body.senha) {
       return { statusCode: 400 }
     }
   }
--- src/presentation/routers/login-controller.spec.js
+++ src/presentation/routers/login-controller.spec.js
@@ -1,11 +1,24 @@
 const makeLoginController = require('./login-controller')
 
-describe('Login router', () => {
-  test('Retorn 400 se não for enviado email', async () => {
+describe('Login controller', () => {
+  test('Retorna 400 se não for enviado email', async () => {
     const loginController = makeLoginController()
     const httpRequest = {
       body: {
         senha: 'senha'
+      }
+    }
+
+    const response = await loginController(httpRequest)
+
+    expect(response.statusCode).toBe(400)
+  })
+
+  test('Retorna 400 se não for enviada senha', async () => {
+    const loginController = makeLoginController()
+    const httpRequest = {
+      body: {
+        email: 'email_qualquer'
       }
     }
 
